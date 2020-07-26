00fc8a35c56972aaf11d95d6bd7fff51a18f2cb7
Allyssa Pascual <pascualallyssa@yahoo.com>
2020-07-26 20:51:14 +0100

    solved problem 16

---
+++ power-digit-sum/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Numerics;
+
+namespace power_digit_sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger exp = (BigInteger) Math.Pow(2, 1000);
+            BigInteger result = 0;
+
+            while (exp > 0) {
+                result += (exp % 10);
+                exp /= 10;
+            }
+            Console.WriteLine($"{result}");
+        }
+    }
+}
