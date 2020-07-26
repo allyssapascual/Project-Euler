ec8aabe59cec73d84600659cfe4a3533ad08e922
Allyssa Pascual <pascualallyssa@yahoo.com>
2020-07-26 19:48:43 +0100

    solved problem 15

---
+++ lattice-paths/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace lattice_paths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 20;
+            long path = 1;
+
+            for (int i = 0; i < size; i++) {
+                path *= (2 * size) - i;
+                path /= i + 1;
+            }
+
+            Console.WriteLine(path);
+        }
+    }
+}
