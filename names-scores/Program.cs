de023d506ef549b1b0e4ff3054d6e59b7822d78c
Allyssa Pascual <pascualallyssa@yahoo.com>
2020-07-28 19:27:45 +0100

    solved problem 22

---
+++ names-scores/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace names_scores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = readFile();
+            Array.Sort(names);
+            int sum = 0;
+
+            for (int i = 0; i < names.Length; i++) {
+                sum += (i + 1) * getScore(names[i]);
+            }
+
+            Console.WriteLine(sum);
+        }
+        static string[] readFile() {
+            StreamReader rdr = new StreamReader("names.txt");
+            string str = rdr.ReadToEnd();
+            rdr.Close();
+
+            string[] names = str.Split(',');
+
+            for (int i = 0; i < names.Length; i++) {
+                names[i] = names[i].Trim('"');
+            }
+
+            return names;
+        }
+
+        static int getScore(string name) {
+            int sum = 0;
+
+            for (int i = 0; i < name.Length; i++) {
+                sum += Convert.ToInt32(name[i]) - 64;
+            } 
+
+            return sum;
+        }
+    }
+}
