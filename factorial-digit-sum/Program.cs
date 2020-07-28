d9afd5a97207b0f4ca8397ff0fb0436d610d3f7a
Allyssa Pascual <pascualallyssa@yahoo.com>
2020-07-28 15:23:52 +0100

    solved problem 20

---
+++ factorial-digit-sum/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Numerics;
+
+namespace factorial_digit_sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger num = factorial(100);
+            Console.WriteLine(num);
+            BigInteger sum = 0;
+
+            while (num > 0) {
+                sum += (num % 10);
+                num /= 10;
+            }
+            Console.WriteLine(sum);
+        }
+
+        static BigInteger factorial(int num) {
+            if (num == 1) {
+                return 1;
+            } else {
+                return num * factorial(num -1);
+            }
+        }
+    }
+}
